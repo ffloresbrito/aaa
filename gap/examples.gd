6e5cdee80998ac548fd13a50de9d825ac0ec6174
Luke <le27@st-andrews.ac.uk>
2019-08-07 09:07:38 +0100

    adds L3Examples

--- gap/examples.gd
+++ gap/examples.gd
@@ -1,3 +1,4 @@
 DeclareOperation("CoreExamples", [IsPosInt]);
 DeclareOperation("L2Examples", [IsPosInt]);
+DeclareOperation("L3Examples", [IsPosInt]);
 DeclareOperation("L24BlockTransducers", [IsPosInt]);
