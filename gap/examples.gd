1ac0b59fc8ebd024199429c1f554c995ae7a40c2
Luke <le27@st-andrews.ac.uk>
2019-08-13 17:29:41 +0100

    adds L2SmallCommutators

--- gap/examples.gd
+++ gap/examples.gd
@@ -2,3 +2,4 @@ DeclareOperation("CoreExamples", [IsPosInt]);
 DeclareOperation("L2Examples", [IsPosInt]);
 DeclareOperation("L3Examples", [IsPosInt]);
 DeclareOperation("L24BlockTransducers", [IsPosInt]);
+DeclareOperation("L2SmallCommutators", [IsPosInt]);
